a4a6e0cb8c295933b2d75122fd98f412ca50c82d
Artur Politidi <frost-128@mail.ru>
2021-10-24 21:32:09 +0600

    updated

--- popup box/cgi-bin/handling.py
+++ popup box/cgi-bin/handling.py
@@ -7,7 +7,7 @@ import os
 form = cgi.FieldStorage()
 email = form.getfirst('email', 'не задано')
 
-os.system(f'python3 sender.py')
+# os.system(f'python3 sender.py')
 
 print("Content-type: text/html; charset=utf-8\n")
 print(f'<p>На вашу почту {email} отправлено письмо. Пожалуйста подтвердите его.</p>')
